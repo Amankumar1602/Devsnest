cee4a31e166d4b9ea1213a203816c30bc7bfa582
Amankumar1602 <79586505+Amankumar1602@users.noreply.github.com>
2021-08-03 15:43:52 +0530

    Create counter.jsx

---
+++ Frontend/src/Day_19/components/counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  var [counter, setCounter] = useState(0);
+  let myStyle = {
+    display: "inline-block",
+  };
+  return (
+    <div className="container" style={myStyle}>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        {counter}
+      </button>
+    </div>
+  );
+}
