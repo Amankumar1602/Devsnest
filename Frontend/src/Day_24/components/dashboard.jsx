559c3319ebaf67a477c5ec904d7ee23466150620
Amankumar1602 <79586505+Amankumar1602@users.noreply.github.com>
2021-08-09 19:40:32 +0530

    Create dashboard.jsx

---
+++ Frontend/src/Day_24/components/dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl mt-4">Dashboard: Private Route</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
