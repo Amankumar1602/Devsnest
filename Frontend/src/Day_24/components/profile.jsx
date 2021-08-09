4ee10972fcb8fd820ba441ea397bfe6746e75ebb
Amankumar1602 <79586505+Amankumar1602@users.noreply.github.com>
2021-08-09 19:42:10 +0530

    Create profile.jsx

---
+++ Frontend/src/Day_24/components/profile.jsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl mt-4">Profile: Private Route</h2>
+    </div>
+  );
+};
+
+export default Profile;
